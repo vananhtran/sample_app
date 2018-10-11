a29ad74644837b5ae828efe98246d53ce3c52db3
nhatvytran <nhatvy2356@gmail.com>
2018-10-11 08:18:08 +0700

    chap8

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
-  validates :name,  presence: true, length: {maximum: Settings.users.max_name}
+  validates :name, presence: true, length: {maximum: Settings.users.max_name}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: Settings.users.max_email},
-    format: {with: VALID_EMAIL_REGEX},uniqueness: {case_sensitive: false}
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, presence: true, length: {minimum: Settings.users.min_pass}
+
+  def User.digest string
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create string, cost: cost
+  end
 
   private
 
